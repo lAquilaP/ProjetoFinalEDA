c0e6b5e08bcc2835855695ca43b50b71b1db9c84
guilherme-bezerra-castro <guilhermebezerradecastro@gmail.com>
2026-06-20 13:35:06 -0300

    feat: implementa estrutura do nó e protótipos das funções

---
+++ rubro_negra.h
@@ -0,0 +1,19 @@
+#include "csv.h"
+
+typedef struct NO *rubro_negra;
+
+struct NO{
+    Funcionario info;
+    struct NO *esq;
+    struct NO *dir;
+    int cor;
+};
+
+int corNo(rubro_negra H);
+void trocaCor(rubro_negra H);
+rubro_negra rotacaoEsq(rubro_negra H);
+rubro_negra rotacaoDireita(rubro_negra H);
+
+rubro_negra inserirRubroNegra(rubro_negra H, Funcionario f);
+void imprimirRubroNegra(rubro_negra H);
+void liberarRubroNegra(rubro_negra H);
